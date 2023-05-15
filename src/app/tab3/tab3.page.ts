f32f994bc5a5a6630f01d45294984c3abc5a5186
DanielSantos <danielsantos633a@gmail.com>
2023-05-15 09:46:30 -0300

    Programa pronto segundo cmmit

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
 
-  constructor() {}
+  form!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    //private contatoService: ContatoService,
+    public toastController: ToastController
+  ) {}
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      nome: ['', [Validators.required]],
+      endereco: ['', [Validators.required]],
+    });
+  }
+
+  enviar() {
+    let nome = this.form.get('nome')?.value;
+    let endereco = this.form.get('endereco')?.value;
+
+    this.abrirUrl(
+      `https://api.whatsapp.com/send?phone&text=Ol%C3%A1%21+Meu+nome+%C3%A9+${nome}+moro+em+${endereco}+e+vim+atrav%C3%A9s+do+seu+aplicativo+e+gostaria+de+receber+doa%C3%A7%C3%B5es.`);    
+  }
+
+  abrirUrl(url: string) {
+    window.open(url, '_blank')?.focus();
+  }
 
 }
